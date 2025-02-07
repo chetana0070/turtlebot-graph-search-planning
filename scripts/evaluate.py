029e9457adb6cf1b2dd00ab60e95c5f3d7f50d0a
Che-ta-na <cchakrap@asu.edu>
2025-02-07 00:24:17 -0700

    evaluate.py
    the updated and modified evaluate.py code

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,164 +1,99 @@
-import problem 
+import problem
 from node import Node
 from priority_queue import PriorityQueue
 import time
+import math
 
 class SearchTimeOutError(Exception):
     pass
 
-def compute_g(algorithm, node, goal_state):
+def compute_g(algorithm, node):
     """
-        Evaluates the g() value.
-
-        Parameters
-        ===========
-            algorithm: str
-                The algorithm type based on which the g-value will be computed.
-            node: Node
-                The node whose g-value is to be computed.
-            goal_state: State
-                The goal state for the problem.
-
-        Returns
-        ========
-            int
-                The g-value for the node.
+    Computes the g() value based on the algorithm type.
     """
-
-    if algorithm == "bfs":
-        return NotImplementedError()
-
-    if algorithm == "astar":
-
-        return NotImplementedError()
-
+    if algorithm in {"astar", "ucs", "custom-astar"}:
+        return node.get_total_action_cost() if node else 0
+    elif algorithm == "bfs":
+        return node.get_depth()
     elif algorithm == "gbfs":
-
-        return NotImplementedError()
-
-    elif algorithm == "ucs":
-
-        return NotImplementedError()
-
-    elif algorithm == "custom-astar":
-
-        return NotImplementedError()
-
-    # Should never reach here.
-    assert False
-    return float("inf")
-
+        return 0
+    raise ValueError("Unknown algorithm type")
 
 def compute_h(algorithm, node, goal_state):
     """
-        Evaluates the h() value.
-
-        Parameters
-        ===========
-            algorithm: str
-                The algorithm type based on which the h-value will be computed.
-            node: Node
-                The node whose h-value is to be computed.
-            goal_state: State
-                The goal state for the problem.
-
-        Returns
-        ========
-            int
-                The h-value for the node.
+    Computes the h() value based on the algorithm type.
     """
-
-    if algorithm == "bfs":
-        
-        return NotImplementedError()
-
-    if algorithm == "astar":
-
-        return NotImplementedError()
-
-    elif algorithm == "gbfs":
-
-        return NotImplementedError()
-
-    elif algorithm == "ucs":
-        
-        return NotImplementedError()
-
+    if algorithm in {"bfs", "ucs"}:
+        return 0
+    elif algorithm in {"astar", "gbfs"}:
+        return get_manhattan_distance(node.get_state(), goal_state)
     elif algorithm == "custom-astar":
-
-        return NotImplementedError()
-
-    # Should never reach here.
-    assert False
-    return float("inf")
-
+        return get_custom_heuristic(node.get_state(), goal_state)
+    raise ValueError("Unknown algorithm type")
 
 def get_manhattan_distance(from_state, to_state):
     return abs(from_state.x - to_state.x) + abs(from_state.y - to_state.y)
 
-
 def get_custom_heuristic(from_state, to_state):
+    x_diff, y_diff = abs(from_state.x - to_state.x), abs(from_state.y - to_state.y)
+    return (x_diff + y_diff) + ((math.sqrt(2) - 2) * min(x_diff, y_diff))
+
+def get_plan(goal_node):
+    """
+    Constructs the action sequence from the goal node back to the start.
+    """
+    actions = []
+    while goal_node and goal_node.get_parent():
+        actions.append(goal_node.get_action())
+        goal_node = goal_node.get_parent()
+    return list(reversed(actions))
+
+def run_algorithm(algorithm, helper, priority_queue, goal_state):
+    """
+    Executes the search algorithm.
+    """
+    visited_states = {}
+    total_nodes_expanded = 0
     
-    return NotImplementedError()
+    while not priority_queue.is_empty():
+        current_node = priority_queue.pop()
+        current_state = current_node.get_state()
+        
+        if current_state == goal_state or helper.is_goal_state(current_state):
+            return get_plan(current_node), total_nodes_expanded
+        
+        if current_state not in visited_states:
+            visited_states[current_state] = True
+            successors = helper.get_successor(current_state)
+            
+            if successors:
+                total_nodes_expanded += 1
+                for action, (state, cost) in successors.items():
+                    if state.get_x() >= 0.0 and state.get_y() >= 0.0:
+                        state_node = Node(state, current_node, current_node.get_depth() + 1, action, cost)
+                        g_value = compute_g(algorithm, state_node)
+                        h_value = compute_h(algorithm, state_node, goal_state)
+                        priority_queue.push(g_value + h_value, state_node)
+    
+    return [], total_nodes_expanded
 
 def graph_search(algorithm, time_limit):
     """
-        Performs a search using the specified algorithm.
-        
-        Parameters
-        ===========
-            algorithm: str
-                The algorithm to be used for searching.
-            time_limit: int
-                The time limit in seconds to run this method for.
-                
-        Returns
-        ========
-            tuple(list, int)
-                A tuple of the action_list and the total number of nodes
-                expanded.
+    Performs a search using the specified algorithm.
     """
-    
-    # The helper allows us to access the problem functions.
     helper = problem.Helper()
+    init_state, goal_state = helper.get_initial_state(), helper.get_goal_state()[0]
     
-    # Get the initial and the goal states.
-    init_state = helper.get_initial_state()
-    goal_state = helper.get_goal_state()[0]
-
-    #Initialize the init node of the search tree and compute its f_score
     init_node = Node(init_state, None, 0, None, 0)
-    f_score = compute_g(algorithm, init_node, goal_state) \
-        + compute_h(algorithm, init_node, goal_state)
-
-       
-    # Initialize the fringe as a priority queue.
+    f_score = compute_g(algorithm, init_node) + compute_h(algorithm, init_node, goal_state)
+    
     priority_queue = PriorityQueue()
     priority_queue.push(f_score, init_node)
     
-
-    # action_list should contain the sequence of actions to execute to reach from init_state to goal_state
-    action_list = []
-
-    # total_nodes_expanded maintains the total number of nodes expanded during the search
-    total_nodes_expanded = 0
-    time_limit = time.time() + time_limit
+    end_time = time.time() + time_limit
+    action_list, total_nodes_expanded = run_algorithm(algorithm, helper, priority_queue, goal_state)
     
-    '''
-    YOUR CODE HERE
-
-    Remove "raise NotImplementedError() and add your code.
-
-    Your code for graph search should populate action_list and set total_nodes_expanded
-    The automated script will verify their values
-
-    In addition to this you must also write code for:
-    1. compute_g
-    2. compute_h
-    '''
-
-    if time.time() >= time_limit:
+    if time.time() >= end_time:
+        raise SearchTimeOutError(f"Search timed out after {time_limit} seconds.")
     
-        raise SearchTimeOutError("Search timed out after %u secs." % (time_limit))
-
     return action_list, total_nodes_expanded
